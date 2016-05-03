79b7e4e47b15a71f24f206c625f3f75787a4ab74
kamalasaurus <kamalasaurus@gmail.com>
2016-05-03 04:26:10 -0400

    added nav structure

---
+++ app/lib/Nav.js
@@ -0,0 +1,18 @@
+import m from 'mithril';
+
+export default class Nav {
+
+  constructor (contents = []) {
+    // contents must be m() components
+    this.contents = contents;
+    this.controller = ()=> {
+      return this;
+    }
+  }
+
+  view (controller, args) {
+    return m('div.app', [m('nav.main-nav')].concat(controller.contents));
+  }
+
+}
+
--- app/lib/main.js
+++ app/lib/main.js
@@ -1,11 +1,12 @@
 import m from 'mithril';
+import Nav from './Nav';
 import WaiverForm from './WaiverForm';
 
-// eventually of the form new Nav(new PageElement())
+// eventually of the form new Nav([new PageElement(), ...])
 // modals w/ query parameters
 // fix trailing question-mark
 m.route(document.body, '/', {
-  '/': new WaiverForm()
+  '/': new Nav([new WaiverForm()]),
 });
 
 //https://github.com/airbnb/javascript this is a good ES6+ style-guide
